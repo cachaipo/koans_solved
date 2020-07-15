c1208eab95510e75ac3792b734e952a0dff6fd2c
cachaipo <62940001+cachaipo@users.noreply.github.com>
2020-07-15 12:48:39 +0200

    add solved koans

---
+++ koans/about_scoring_project.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from runner.koan import *
+
+# Greed is a dice game where you roll up to five dice to accumulate
+# points.  The following "score" function will be used calculate the
+# score of a single roll of the dice.
+#
+# A greed roll is scored as follows:
+#
+# * A set of three ones is 1000 points
+#
+# * A set of three numbers (other than ones) is worth 100 times the
+#   number. (e.g. three fives is 500 points).
+#
+# * A one (that is not part of a set of three) is worth 100 points.
+#
+# * A five (that is not part of a set of three) is worth 50 points.
+#
+# * Everything else is worth 0 points.
+#
+#
+# Examples:
+#
+# score([1,1,1,5,1]) => 1150 points
+# score([2,3,4,6,2]) => 0 points
+# score([3,4,5,3,3]) => 350 points
+# score([1,5,1,2,4]) => 250 points
+#
+# More scoring examples are given in the tests below:
+#
+# Your goal is to write the score method.
+
+def score(dice):
+    # You need to write this method
+    '''if len(dice) == 0:
+        return 0
+    if len(dice) == 3 and dice[0] == dice[1] == dice[2]:
+        if dice == [1, 1, 1]:
+            return 1000
+        else:
+            return dice[0] * 100
+            
+    if dice.count(number) == 3:
+            if number == 1:
+                result += 1000                
+            else:
+                result += number * 100
+            dice.remove(number)
+            dice.remove(number)
+            dice.remove(number)        
+            
+            
+            '''
+    
+    
+    count_1 = 0
+    count_2 = 0
+    count_3 = 0
+    count_4 = 0
+    count_5 = 0
+    count_6 = 0
+    result = 0
+    for number in dice:
+        if len(dice) == 0:
+            return result
+        if number == 1:
+            count_1 += 1
+            result += 100
+            if count_1 == 3:
+                result = result + 1000 - 300
+        if number == 2:
+            count_2 += 1
+            if count_2 == 3:
+                result += 200
+        if number == 3:
+            count_3 += 1
+            if count_3 == 3:
+                result += 300
+        if number == 4:
+            count_4 += 1
+            if count_4 == 3:
+                result += 400
+        if number == 5:
+            count_5 += 1
+            result += 50
+            if count_5 == 3:
+                result += 350
+        if number == 6:
+            count_6 += 1
+            if count_6 == 3:
+                result += 600   
+    return result
+
+
+class AboutScoringProject(Koan):
+    def test_score_of_an_empty_list_is_zero(self):
+        self.assertEqual(0, score([]))
+
+    def test_score_of_a_single_roll_of_5_is_50(self):
+        self.assertEqual(50, score([5]))
+
+    def test_score_of_a_single_roll_of_1_is_100(self):
+        self.assertEqual(100, score([1]))
+
+    def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores(self):
+        self.assertEqual(300, score([1,5,5,1]))
+
+    def test_score_of_single_2s_3s_4s_and_6s_are_zero(self):
+        self.assertEqual(0, score([2,3,4,6]))
+
+    def test_score_of_a_triple_1_is_1000(self):
+        self.assertEqual(1000, score([1,1,1]))
+
+    def test_score_of_other_triples_is_100x(self):
+        self.assertEqual(200, score([2,2,2]))
+        self.assertEqual(300, score([3,3,3]))
+        self.assertEqual(400, score([4,4,4]))
+        self.assertEqual(500, score([5,5,5]))
+        self.assertEqual(600, score([6,6,6]))
+
+    def test_score_of_mixed_is_sum(self):
+        self.assertEqual(250, score([2,5,2,2,3]))
+        self.assertEqual(550, score([5,5,5,5]))
+        self.assertEqual(1150, score([1,1,1,5,1]))
+
+    def test_ones_not_left_out(self):
+        self.assertEqual(300, score([1,2,2,2]))
+        self.assertEqual(350, score([1,5,2,2,2]))
